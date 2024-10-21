bdf611f843aadc0c45796d57103260895ecbd694
Stefano Imperial Salvador <105392252+is-stefano@users.noreply.github.com>
2024-10-21 18:21:26 +0100

    Update flux.js 2.0

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,49 +1,47 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-
-			host: "https://playground.4geeks.com/contact/",
-			slug: "",
-			agenda: [],
-			demo: [
-				{ title: "FIRST", background: "white", initial: "white" },
-				{ title: "SECOND", background: "white", initial: "white" }
-			],
-			cohorte: 'Spain 72',
-			user: '',
-			host: 'https://playground.4geeks.com/contact',
-			characters: [],
-			starships: [],
-			planets: [],
-			species: [],
-			currentStarship: {},
-			currentCharacter: [],
-			currentPlanet: [],
-			currentSpecies: [],
-			alert: {
-				visible: true,
-				back: 'danger',
-				text: 'User not exist'
-			},
-			contacts: [],
-			isAgenda: false,
-			currentContact: {},
-			characters: [],
-			character: [],
-			starships: [],
-			starship: [],
-			planets: [],
-			planet: [],
-			favorites: [],
-			newContact: {
-				"name": "",
-				"phone": "",
-				"email": "",
-				"address": ""
-			},
-
-		},
+    return {
+        store: {
+            message: null,
+            host: "https://playground.4geeks.com/contact/",
+            slug: "",
+            agenda: [],
+            demo: [
+                { title: "FIRST", background: "white", initial: "white" },
+                { title: "SECOND", background: "white", initial: "white" }
+            ],
+            cohorte: 'Spain 72',
+            user: '',
+            host: 'https://playground.4geeks.com/contact',
+            characters: [],
+            starships: [],
+            planets: [],
+            species: [],
+            currentStarship: {},
+            currentCharacter: [],
+            currentPlanet: [],
+            currentSpecies: [],
+            alert: {
+                visible: true,
+                back: 'danger',
+                text: 'User not exist'
+            },
+            contacts: [],
+            isAgenda: false,
+            currentContact: {},
+            characters: [],
+            character: [],
+            starships: [],
+            starship: [],
+            planets: [],
+            planet: [],
+            favorites: [],
+            newContact: {
+                "name": "",
+                "phone": "",
+                "email": "",
+                "address": ""
+            },
+        },
 		actions: {
 			getCharacters: async () => {
 				const url = `${process.env.BACKEND_URL}/api/people`
@@ -73,6 +71,50 @@ const getState = ({ getStore, getActions, setStore }) => {
 				setStore({ character: data.result });
 			},
 			setCharacter: (person) => { setStore({character: person})},
+			addCharacterFavorite: async (people_id) => {
+			    const store = getStore();
+			    const url = `${process.env.BACKEND_URL}/favorite/character/${character_id}`;
+			    const options = {
+			        method: "POST",
+			        headers: {
+			            "Content-Type": "application/json"
+			        }
+			    };
+			    try {
+			        const response = await fetch(url, options);
+			        if (!response.ok) {
+			            console.log('Error al agregar el personaje favorito:', response.status, response.statusText);
+			            return;
+			        }
+			        const data = await response.json();
+			        console.log("Personaje añadido a favoritos:", data);
+			        setStore({ favorites: [...store.favorites, data] });
+			    } catch (error) {
+			        console.error('Error:', error);
+			    }
+			},
+			removeCharacterFavorite: async (people_id) => {
+			    const store = getStore();
+			    const url = `${process.env.BACKEND_URL}/favorite/character/${character_id}`;
+			    const options = {
+			        method: "DELETE",
+			        headers: {
+			            "Content-Type": "application/json"
+			        }
+			    };
+			    try {
+			        const response = await fetch(url, options);
+			        if (!response.ok) {
+			            console.log('Error al eliminar el personaje favorito:', response.status, response.statusText);
+			            return;
+			        }
+			        const data = await response.json();
+			        console.log("Personaje eliminado de favoritos:", data);
+			        setStore({ favorites: store.favorites.filter(fav => fav.id !== people_id) }); 
+			    } catch (error) {
+			        console.error('Error:', error);
+			    }
+			},
 			getPlanets: async () => {
 				const url = `${process.env.BACKEND_URL}/api/planets`
 				const options = {
@@ -101,6 +143,50 @@ const getState = ({ getStore, getActions, setStore }) => {
 				setStore({ planet: data.result });
 			},
 			setPlanet: (star) => { setStore({planet: star})},
+			addPlanetFavorite: async (planet_id) => {
+			    const store = getStore();
+			    const url = `${process.env.BACKEND_URL}/favorite/planet/${planet_id}`;
+			    const options = {
+			        method: "POST",
+			        headers: {
+			            "Content-Type": "application/json"
+			        }
+			    };
+			    try {
+			        const response = await fetch(url, options);
+			        if (!response.ok) {
+			            console.log('Error al agregar el planeta favorito:', response.status, response.statusText);
+			            return;
+			        }
+			        const data = await response.json();
+			        console.log("Planeta añadido a favoritos:", data);
+			        setStore({ favorites: [...store.favorites, data] }); 
+			    } catch (error) {
+			        console.error('Error:', error);
+			    }
+			},
+			removePlanetFavorite: async (planet_id) => {
+			    const store = getStore();
+			    const url = `${process.env.BACKEND_URL}/favorite/planet/${planet_id}`;
+			    const options = {
+			        method: "DELETE",
+			        headers: {
+			            "Content-Type": "application/json"
+			        }
+			    };
+			    try {
+			        const response = await fetch(url, options);
+			        if (!response.ok) {
+			            console.log('Error al eliminar el planeta favorito:', response.status, response.statusText);
+			            return;
+			        }
+			        const data = await response.json();
+			        console.log("Planeta eliminado de favoritos:", data);
+			        setStore({ favorites: store.favorites.filter(fav => fav.id !== planet_id) }); 
+			    } catch (error) {
+			        console.error('Error:', error);
+			    }
+			},
 			getStarships: async () => {
 				const url = `${process.env.BACKEND_URL}/api/starships`
 				const options = {
